5243bb30e646f7325bac518141854d6f9539ab0a
unknown <gabrieltorresmora2@gmail.com>
2023-01-19 08:23:13 -0300

    Finalizando seccion unaCard del proyecto agregando una barra de progreso, falta por finalizar libro de cartas

--- src/components/multipleLlamadasApi/multipleLlamadaApiComponent.jsx
+++ src/components/multipleLlamadasApi/multipleLlamadaApiComponent.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 export const MultipleLlamadaApiComponent = () => {
 
     const [condicion, setCondicion] = useState(false);
-    
 
     const getData = (id) =>{
         fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
--- src/components/unaCard/unaCardComponent.jsx
+++ src/components/unaCard/unaCardComponent.jsx
@@ -11,12 +11,11 @@ export const UnaLlamadaAlaApiDePokemonComponent = () => {
          .then((res) => res.json())
          .then((data) => {
             enviandoLosDatosAlDom(data)
-         })
-
-       
+         }) 
 
     const enviandoLosDatosAlDom = (data) => {
 
+        console.log(data)
         const contenedorCard = document.querySelector('.contenedorCard');
 
         contenedorCard.style.display = 'block';
@@ -32,24 +31,81 @@ export const UnaLlamadaAlaApiDePokemonComponent = () => {
         defensaPokemon = document.querySelector('.defensaPokemon'),
         specialAttackPokemon = document.querySelector('.specialAttackPokemon'),
         specialDefensePokemon = document.querySelector('.specialDefensePokemon'),
-        speedPokemon = document.querySelector('.speedPokemon');
+        speedPokemon = document.querySelector('.speedPokemon'),
+        progressBarPokemonOne = document.querySelector('.progressBarPokemonOne'),
+        progressBarPokemonTwo = document.querySelector('.progressBarPokemonTwo'),
+        progressBarPokemonThree = document.querySelector('.progressBarPokemonThree'),
+        progressBarPokemonFour = document.querySelector('.progressBarPokemonFour'),
+        progressBarPokemonFive = document.querySelector('.progressBarPokemonFive'),
+        progressBarPokemonSix = document.querySelector('.progressBarPokemonSix'),
+        progressBarPokemonSeven = document.querySelector('.progressBarPokemonSeven'),
+        progressBarPokemonEight = document.querySelector('.progressBarPokemonEight');
         
 
         /* ANIDANDO LOS ELEMENTOS DE LA API AL DOM DE MI PAGINA */
         imagenPokemon.src = data.sprites.other.home.front_default;
         namePokemon.textContent = data.name[0].toUpperCase() + data.name.substring(1);
         numberPokemon.textContent = `Tarjeta: #${data.id.toString().padStart(3,0)}`;
-        baseDeExperiencia.textContent = `Experiencia: ${data.base_experience}`;
-        pesoPokemon.textContent = `Peso: ${data.weight}`;
-        hpPokemon.textContent = `HP: ${data.stats[0].base_stat}`
-        ataquePokemon.textContent = `Ataque: ${data.stats[1].base_stat}`
-        defensaPokemon.textContent = `Denfesa: ${data.stats[2].base_stat}`
-        specialAttackPokemon.textContent = `Especial-Ataque: ${data.stats[3].base_stat}`
-        specialDefensePokemon.textContent = `Especial-Defensa: ${data.stats[4].base_stat}`
-        speedPokemon.textContent = `Velocidad: ${data.stats[5].base_stat}`
 
-    }
-        
+        baseDeExperiencia.textContent = `Experiencia: ${data.base_experience}`;
+        progressBarPokemonOne.setAttribute("aria-valuenow", data.base_experience)
+        progressBarPokemonOne.setAttribute("aria-valuemin", 0);
+        progressBarPokemonOne.setAttribute('aria-valuemax', 300);
+        progressBarPokemonOne.style.width =  `${data.base_experience}px`;
+        progressBarPokemonOne.textContent = data.base_experience;
+
+        pesoPokemon.textContent = `Peso: ${data.weight}`;
+        progressBarPokemonTwo.setAttribute('aria-valuenow', data.weight)
+        progressBarPokemonTwo.setAttribute('aria-valuemin', 0);
+        progressBarPokemonTwo.setAttribute('aria-valuemax', 1000);
+        progressBarPokemonTwo.style.width = `${data.weight}px`;
+        progressBarPokemonTwo.textContent = data.weight;
+
+        hpPokemon.textContent = `HP: ${data.stats[0].base_stat}`
+        progressBarPokemonThree.setAttribute('aria-valuenow', data.stats[0].base_stat)
+        progressBarPokemonThree.setAttribute('aria-valuemin', 0);
+        progressBarPokemonThree.setAttribute('aria-valuemax', 200);
+        progressBarPokemonThree.style.width = `${data.stats[0].base_stat}px`;
+        progressBarPokemonThree.textContent = data.stats[0].base_stat;
+
+
+
+        ataquePokemon.textContent = `Ataque: ${data.stats[1].base_stat}`
+        progressBarPokemonFour.setAttribute('aria-valuenow', data.stats[1].base_stat)
+        progressBarPokemonFour.setAttribute('aria-valuemin', 0);
+        progressBarPokemonFour.setAttribute('aria-valuemax', 150);
+        progressBarPokemonFour.style.width = `${data.stats[1].base_stat}px`;
+        progressBarPokemonFour.textContent = data.stats[1].base_stat;
+
+        defensaPokemon.textContent = `Denfesa: ${data.stats[2].base_stat}`
+        progressBarPokemonFive.setAttribute('aria-valuenow', data.stats[2].base_stat)
+        progressBarPokemonFive.setAttribute('aria-valuemin', 2);
+        progressBarPokemonFive.setAttribute('aria-valuemax', 150);
+        progressBarPokemonFive.style.width = `${data.stats[2].base_stat}px`;
+        progressBarPokemonFive.textContent = data.stats[2].base_stat;
+
+        specialAttackPokemon.textContent = `Especial-Ataque: ${data.stats[3].base_stat}`
+        progressBarPokemonSix.setAttribute('aria-valuenow', data.stats[3].base_stat)
+        progressBarPokemonSix.setAttribute('aria-valuemin', 0);
+        progressBarPokemonSix.setAttribute('aria-valuemax', 200);
+        progressBarPokemonSix.style.width = `${data.stats[3].base_stat}px`;
+        progressBarPokemonSix.textContent = data.stats[3].base_stat;
+
+        specialDefensePokemon.textContent = `Especial-Defensa: ${data.stats[4].base_stat}`
+        progressBarPokemonSeven.setAttribute('aria-valuenow', data.stats[4].base_stat)
+        progressBarPokemonSeven.setAttribute('aria-valuemin', 0);
+        progressBarPokemonSeven.setAttribute('aria-valuemax', 200);
+        progressBarPokemonSeven.style.width = `${data.stats[4].base_stat}px`;
+        progressBarPokemonSeven.textContent = data.stats[4].base_stat;
+
+        speedPokemon.textContent = `Velocidad: ${data.stats[5].base_stat}`
+        progressBarPokemonEight.setAttribute('aria-valuenow', data.stats[5].base_stat)
+        progressBarPokemonEight.setAttribute('aria-valuemin', 0);
+        progressBarPokemonEight.setAttribute('aria-valuemax', 200);
+        progressBarPokemonEight.style.width = `${data.stats[5].base_stat}px`;
+        progressBarPokemonEight.textContent = data.stats[5].base_stat;
+
+        }
     }
 
     
@@ -69,21 +125,21 @@ export const UnaLlamadaAlaApiDePokemonComponent = () => {
                         <h2 className="namePokemon">Esperando...</h2>
                             <p className="numberPokemon">Esperando...</p> 
                             <p className="baseDeExperiencia">Esperando...</p>
-                            <progress max={200} value={112}></progress>
+                            <div className="progressBarContenedor"><div className="progressBarPokemonOne"></div></div>
                             <p className="pesoPokemon">Esperando...</p>
-                            <progress max={200} value={112}></progress>
+                            <div className="progressBarContenedor"><div className="progressBarPokemonTwo"></div></div>
                             <p className="hpPokemon">Esperando...</p>
-                            <progress max={200} value={112}></progress>
+                            <div className="progressBarContenedor"><div className="progressBarPokemonThree"></div></div>
                             <p className="ataquePokemon">Esperando...</p>
-                            <progress max={200} value={112}></progress>
+                            <div className="progressBarContenedor"><div className="progressBarPokemonFour"></div></div>
                             <p className="defensaPokemon">Esperando...</p>
-                            <progress max={200} value={112}></progress>
+                            <div className="progressBarContenedor"><div className="progressBarPokemonFive"></div></div>
                             <p className="specialAttackPokemon">Esperando...</p>
-                            <progress max={200} value={112}></progress>
+                            <div className="progressBarContenedor"><div className="progressBarPokemonSix"></div></div>
                             <p className="specialDefensePokemon">Esperando...</p>
-                            <progress max={200} value={112}></progress>
+                            <div className="progressBarContenedor"><div className="progressBarPokemonSeven"></div></div>
                             <p className="speedPokemon">Esperando...</p>
-                            <progress max={200} value={112}></progress>
+                            <div className="progressBarContenedor"><div className="progressBarPokemonEight"></div></div>
                     </div>
                 </div>
            </div>
